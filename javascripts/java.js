a75e5cb4bbe22a7ad41537e6b207138f9afeb340
Thai Pham <minh-thai@hotmail.com>
2016-05-01 16:26:55 +0200

    Update java.js

--- javascripts/java.js
+++ javascripts/java.js
@@ -1,30 +1,63 @@
 $(document).ready(function(){
-	$("img").animate({
-		opacity:.5
-	});
-	$('img').hover(function(){
+  $("img").animate({
+    opacity:.5
+  });
+  $('img').hover(function(){
        $(this).stop().animate({opacity:1});
 
-	 },function() {
-	$(this).stop().animate({opacity:.5});
+   },function() {
+  $(this).stop().animate({opacity:.5});
 
     });
 
-    $(document).ready(function(){
-         alert($('div').attr('title'));
-    });
-
-    $(document).ready(function(){
-    	$('h1').hover(function(){
+   $(document).ready(function(){
+      $('h1').hover(function(){
         $(this).css({"background-color":"red"
           });
-         },function() {
-	    $(this).stop().css({"background-color":"blue"});
+        },function() {
+      $(this).stop().css({"background-color":"blue"});
 
-       });
-    });
+       });     
+       }); 
+});
 
- });
+var media = "software";
+var limitOfSearchResults = 1;
 
-	
- 
+var search = function() {
+  
+  var element = $('#search'); 
+  var searchterm = $(element).attr('value'); 
+
+  
+  $.getJSON("http://itunes.apple.com/search?term=" + searchterm 
+              + "&country=de&limit=" + limitOfSearchResults 
+              + "&media=" + media 
+              + "&callback=?", function(data) {
+    
+    img = data["results"][0].artworkUrl100;
+    
+    $('#img').html('<img src="' + img + '"/>');
+
+  });}
+
+
+var Results = 1;
+
+var pic = function() {
+  
+  var element = $('#pic'); 
+  var searchterm = $(element).attr('value'); 
+
+  $.getJSON("https://itunes.apple.com/search?term=jim+jones&country=ca" + searchterm
+
+              + "&country=de&limit=" + Results 
+              + "&callback=?", function(data) {
+    
+    img = data["results"][0].artworkUrl100;
+    
+    $('#img').html('<img src="' + img + '"/>');
+
+  });}
+
+
